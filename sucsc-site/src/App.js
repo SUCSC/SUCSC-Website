5415f9205b483886598c94280747b265ef23d789
cargon21 <sytnerc@gmail.com>
2022-02-21 21:45:51 -0500

    Initial Push

---
+++ sucsc-site/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='text-5xl bg-blue-500'>
+          hey what's up?
+        </div>
+    </div>
+  );
+}
+
+export default App;
