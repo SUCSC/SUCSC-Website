0c8bf0dd2f1bc5ee51ed03ea8f2b5b9cca01cad7
erikawoloschuk <78872804+erikawoloschuk@users.noreply.github.com>
2022-03-10 13:09:05 -0500

    added background for my page

--- sucsc-site/src/components/us/Erika.js
+++ sucsc-site/src/components/us/Erika.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Erika = () => {
   return (
-    <div>Erika</div>
+    <>
+    <section className='h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex items-center justify-center'>
+        <div>
+          <h1 className='text-8xl'>Erika Woloschuk</h1>
+          <p className='pt-1'>Hi I'm Erika!</p>
+        </div>
+      </section>
+
+      <section className='bg-pink-500 py-48'>
+        <h2 className='text-5xl text-white'>Check Out Some of my Coding Projects</h2>
+      </section>
+  
+    </>
   )
 }
 
