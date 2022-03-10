dda934146266064d2de4d9d3ea34eb5688f461ea
cargon21 <sytnerc@gmail.com>
2022-03-10 13:21:36 -0500

    Carson Changes

--- sucsc-site/src/components/us/Carson.js
+++ sucsc-site/src/components/us/Carson.js
@@ -1,22 +1,20 @@
 import React from 'react'
 import TestProject from './../TestProject'
 
+import heading from './../../svg/heading.svg'
+
 const Carson = () => {
   return (
     <div>
-      <section className='h-screen bg-red-300 flex items-center justify-center'>
-        <div>
-          <h1 className='text-8xl'>Carson Sytner</h1>
-          <h2 className='text-5xl pt-2'>Vice President of SUCSC 😎</h2>
-          <p className='pt-1'>Hey I'm Carson, I enjoy computer science, running, and photography.</p>
-        </div>
+      <section className='h-screen bg-white text-white'>
+       <h1 className='text-9xl text-left'>CARSON SYTNER</h1>
+       <img src={heading} alt="" className='fill-amber-900 stroke-red-400'/>
       </section>
 
       <section className='bg-rose-700 py-48'>
         <h2 className='text-5xl text-white'>Check Out Some of my Coding Projects</h2>
         <TestProject sampleText="Oh! Here is some cool text that is very fun! 🐞"/>
       </section>
-      
     </div>
   )
 }
